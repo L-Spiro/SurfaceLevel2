7fa7c751ebf842fd41bb8a0e9a0c945cd6c4a2cd
L-Spiro <yogurtemperor@gmail.com>
2024-01-26 23:39:39 +0900

    Initial image functionality.

--- Src/Image/SL2Image.cpp
+++ Src/Image/SL2Image.cpp
@@ -17,9 +17,51 @@
 
 namespace sl2 {
 
-	CImage::CImage() {
+	CImage::CImage() :
+		m_stArraySize( 0 ),
+		m_stSlices( 0 ),
+		m_stFaces( 0 ),
+		m_ui32Width( 0 ),
+		m_ui32Height( 0 ) {
 	}
 	CImage::~CImage() {
+		Reset();
+	}
+
+
+	// == Operators.
+	/**
+	 * Move operator.
+	 * 
+	 * \param _iOther The image to copy and destroy.
+	 * \return Returns a reference to this image.
+	 **/
+	CImage & CImage::operator = ( CImage &&_iOther ) {
+		m_vMipMaps = std::move( _iOther.m_vMipMaps );
+		m_stArraySize = _iOther.m_stArraySize;
+		m_stSlices = _iOther.m_stSlices;
+		m_stFaces = _iOther.m_stFaces;
+		m_ui32Width = _iOther.m_ui32Width;
+		m_ui32Height = _iOther.m_ui32Height;
+		_iOther.m_stArraySize = 0;
+		_iOther.m_stSlices = 0;
+		_iOther.m_stFaces = 0;
+		_iOther.m_ui32Width = 0;
+		_iOther.m_ui32Height = 0;
+		return (*this);
+	}
+
+	// == Functions.
+	/**
+	 * Resets the object to scratch.  It can be reused after this.
+	 **/
+	void CImage::Reset() {
+		m_stArraySize = 0;
+		m_stSlices = 0;
+		m_stFaces = 0;
+		m_ui32Width = 0;
+		m_ui32Height = 0;
+		m_vMipMaps = std::vector<std::unique_ptr<std::vector<uint8_t>>>();
 	}
 
 }	// namespace sl2
--- Src/Image/SL2Image.h
+++ Src/Image/SL2Image.h
@@ -14,6 +14,10 @@
 
 #pragma once
 
+#include <cstdint>
+#include <memory>
+#include <vector>
+
 
 namespace sl2 {
 
@@ -33,6 +37,68 @@ namespace sl2 {
 	public :
 		CImage();
 		~CImage();
+
+
+		// == Operators.
+		/**
+		 * Move operator.
+		 * 
+		 * \param _iOther The image to copy and destroy.
+		 * \return Returns a reference to this image.
+		 **/
+		CImage &											operator = ( CImage &&_iOther );
+
+
+		// == Functions.
+		/**
+		 * Resets the object to scratch.  It can be reused after this.
+		 **/
+		void												Reset();
+
+
+		/**
+		 * Gets the final size of a byte buffer to be used as a texture plane.  The plane will be over-allocated by 8 bytes and then rounded up to the nearest 8 bytes.
+		 *	So if a 1-by-1 32-bit tecture is being allocated, 4 will be passed to _sSize, and 16 will be returned ((4+8) -> 16).
+		 * 
+		 * \param _sSize The base size of the byte buffer previously calculated by the texture's width, height, and texel size.
+		 * \return Returns the adjusted size of the texture plane to actually be allocated.
+		 **/
+		static inline size_t								GetActualPlaneSize( size_t _sSize );
+
+		/**
+		 * Allocates N mipmaps.
+		 * 
+		 * \param PARM DESC
+		 * \param PARM DESC
+		 * \return DESC
+		 **/
+
+
+	protected :
+		// == Members.
+		std::vector<std::unique_ptr<std::vector<uint8_t>>>	m_vMipMaps;								/**< The array of mipmaps.  Index 0 is the base level. */
+		size_t												m_stArraySize;							/**< Number of slices in an array.  1 for flat 1D/2D images. */
+		size_t												m_stSlices;								/**< The number of slices in the image. */
+		size_t												m_stFaces;								/**< 1 for normal textures, 6 for cube textures. */
+		uint32_t											m_ui32Width;							/**< The width of the texture. */
+		uint32_t											m_ui32Height;							/**< The height of the texture. */
 	};
+	
+
+
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	// DEFINITIONS
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	// == Functions.
+	/**
+	 * Gets the final size of a byte buffer to be used as a texture plane.  The plane will be over-allocated by 8 bytes and then rounded up to the nearest 8 bytes.
+	 *	So if a 1-by-1 32-bit tecture is being allocated, 4 will be passed to _sSize, and 16 will be returned ((4+8) -> 16).
+	 * 
+	 * \param _sSize The base size of the byte buffer previously calculated by the texture's width, height, and texel size.
+	 * \return Returns the adjusted size of the texture plane to actually be allocated.
+	 **/
+	inline size_t CImage::GetActualPlaneSize( size_t _sSize ) {
+		return ((_sSize + sizeof( uint64_t )) + (sizeof( uint64_t ) - 1)) & ~(sizeof( uint64_t ) - 1);
+	}
 
 }	// namespace sl2
